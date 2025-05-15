3019a42f2be2d6df9d7e9c8e4db27d47298aac4a
Kaushik Saha <kaushiksaha004@gmail.com>
2025-05-15 08:46:40 +0530

    change api

--- app/hooks/useStudies.ts
+++ app/hooks/useStudies.ts
@@ -4,7 +4,7 @@ import { StudyResponse } from '@/types/StudyResponse';
 async function fetchStudiesAPI(): Promise<StudyResponse[]> {
     const res = await fetch('https://studiesapi.tikuntech.com/mf2/user/studies', {
         headers: {
-            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NzEzMDA2MSwianRpIjoiOTg2MWFkMjgtNDNlMC00Y2QxLWFkNWQtNzhjNjgwOTQ0YzUzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3R1c2VyQGV4YW1wbGUuY29tIiwibmJmIjoxNzQ3MTMwMDYxLCJjc3JmIjoiMmZmNzllYjItNTdkNi00ZGE3LTk2ZTYtMWViMDE4ZTJkODZjIiwiZXhwIjoxNzQ5NzIyMDYxfQ.dS5Yw1MYpGtLKoCUjXsK5h5Rt8qsddX7WwINdkjkHm4`
+            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NzI3ODkwNywianRpIjoiMTdlYWIwODctMGNhMC00NzVkLWEwMWQtMmFhYmU5ZjY2NWRkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRlc3RAZ21haWwuY29tIiwibmJmIjoxNzQ3Mjc4OTA3LCJjc3JmIjoiMGIzYzg5YTktZjc1NC00NzE3LTliNDctZjI5ZmE2OWU5N2Q1IiwiZXhwIjoxNzQ5ODcwOTA3fQ.7xfZwtAhLIbcJmJCv7ZOV88fKEUOM0i_75Pz3ALGsiI`
         }
     });
     if (!res.ok) {
